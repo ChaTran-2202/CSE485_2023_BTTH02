a74d7d0e7c3f7f4f45e534c638257bde37f56385
Kuro2202 <tranthihuongcha2k2@gmail.com>
2023-03-05 00:50:33 +0700

    update CategoryCtrl

--- services/CategorySer.php
+++ services/CategorySer.php
@@ -1,21 +1,48 @@
 <?php
+include ("../configs/DbConnection.php");
+include ("../models/Category.php");
 class CategorySer{
     public function getAllCategory(){
-        require_once '../configs/DbConnection.php';
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+
         $sql = "SELECT * FROM theloai";
-        $stmt = $pdo->prepare($sql);
+        $stmt = $conn->prepare($sql);
         $stmt->execute();
-        $category = $stmt->fetchAll();
-        return $category;
-    }
 
-    public function insertCategory(){
-        $sqlIndex = "SELECT COUNT(ma_tloai) FROM theloai";
-        $stmt = $pdo->prepare($sql);
+        $categorylist = [];
+        foreach ($stmt->fetchAll() as $row){
+            $categorylist[] = new Category($row['ma_tloai'], $row['ten_tloai']);
+        }
+        return $categorylist;
+    }
+    public function addCategory($data){
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+
+        $sql = "INSERT INTO theloai (ma_tloai, ten_tloai) VALUES (:id, :name)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('ma_tloai', $data['id'], PDO::PARAM_INT);
+        $stmt->bindValue('ten_tloai', $data['name'], PDO::PARAM_STR);
         $stmt->execute();
-        $endIndex = $stmt->fetch() + 1;
-
-
     }
+    public function updateCategory($data){
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
 
+        $sql = "UPDATE theloai SET ten_tloai = :name WHERE ma_tloai = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('ma_tloai', $data['name'], PDO::PARAM_INT);
+        $stmt->bindValue('ten_tloai', $data['id'], PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    public function delCategory($id){
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+
+        $sql = "DELETE FROM theloai WHERE ma_tloai = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('ten_tloai', $id, PDO::PARAM_STR);
+        $stmt->execute();
+    }
 }

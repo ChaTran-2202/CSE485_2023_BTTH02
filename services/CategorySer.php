fc715a66a39dae59fda5658c7960495661e6ca63
Kuro2202 <tranthihuongcha2k2@gmail.com>
2023-03-04 20:50:50 +0700

    update CategorySer

--- services/CategorySer.php
+++ services/CategorySer.php
@@ -1,1 +1,21 @@
 <?php
+class CategorySer{
+    public function getAllCategory(){
+        require_once '../configs/DbConnection.php';
+        $sql = "SELECT * FROM theloai";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $category = $stmt->fetchAll();
+        return $category;
+    }
+
+    public function insertCategory(){
+        $sqlIndex = "SELECT COUNT(ma_tloai) FROM theloai";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $endIndex = $stmt->fetch() + 1;
+
+
+    }
+
+}

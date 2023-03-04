fc04e0df66c175893630abfa7d416d9ad504a5be
Kuro2202 <tranthihuongcha2k2@gmail.com>
2023-03-04 18:33:37 +0700

    category model

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php
+// URL: localhost/.../CSE485_2023_BTTH02/index.php?controller=A&action=B
+// 1.Check values of controller and action
+
+// 2.Check path of file controller
+// 3.Controller initialization and call action function

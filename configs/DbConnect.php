d21862be865f544c673a56d156a2119922a57a1f
Kuro2202 <tranthihuongcha2k2@gmail.com>
2023-03-04 11:17:24 +0700

    Database connection

---
+++ configs/DbConnect.php
@@ -0,0 +1,16 @@
+<?php
+$type     = 'mysql';
+$server   = 'localhost';
+$db       = 'btth01_cse485';
+$port     = '3306';
+$charset  = 'utf8mb4';
+$username = 'root';
+$password = '';
+
+// DO NOT CHANGE ANYTHING BENEATH THIS LINE
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset"; // Create DSN
+try {                                                               // Try following code
+    $pdo = new PDO($dsn, $username, $password);           // Create PDO object
+} catch (PDOException $e) {                                         // If exception thrown
+    throw new PDOException($e->getMessage(), $e->getCode());        // Re-throw exception
+}
